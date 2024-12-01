ddba58db6c304e1094011c4ac4d5a05d4c3119cd
rached <rachedgalaxy@gmail.com>
2024-12-01 04:09:33 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/react-portfolio-template/',
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['mixed-decls'],
+        base: "/mer444",
+      },
+    },
+  },
+})
